a1566f6c34b92a3801246ce05523bb4f12822870
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-07 22:11:55 +0200

    fixed login issue, must fix log out now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-protected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:update, keys: [:first_name, :last_name, :url])
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   devise_for :accounts
- root to: 'public#main'
+  root to: 'public#main'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
