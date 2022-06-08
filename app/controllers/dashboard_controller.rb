0a255650ec45c2481773128ffe433ecad9825014
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-08 13:58:33 +0200

    fixed logout link

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,10 @@
+class DashboardController < ApplicationController
+  def index
+  end
+
+  def items
+  end
+
+  def reports
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :items
   devise_for :accounts
+  get 'dashboard/index'
+  get 'dashboard/items'
+  get 'dashboard/reports'
+  resources :items
   root to: 'public#main'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
